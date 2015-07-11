4b6d1bd91631cd02b8d711173a2c9ab8905e83ae
Abhilash <abhilash.sixthsense@gmail.com>
2015-07-11 12:03:21 +0530

    Added Java materials

---
+++ JavaMaterial/new  2.java
@@ -0,0 +1,13 @@
+package language.fundamentals;
+
+public class area {
+	public static void main(String[] args) {
+		findarea();
+	}
+
+	public static void findarea() {
+		int d=1;
+		float a;
+		a=3.14*d*d;
+	}
+}
